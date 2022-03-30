20ca2b7d4ee19533a3cd68b5bbc3b6f7d5ea9b1a
snyggme <rcbabahin@gmail.com>
2022-03-30 10:51:19 +0300

    added hooks in sigform component

--- src/Components/SignForm/SignForm.js
+++ src/Components/SignForm/SignForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -9,28 +9,27 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
-class SignForm extends Component {
-    constructor(props) {
-        super(props);
+const SignForm = (props) => {
+    const { route, loadUser, onRouteChange } = props;
+    const [ user, setUser ] = useState({
+        email: '',
+        password: '',
+        name: ''
+    });
+    const [ isLoading, setLoading ] = useState(false);
 
-        this.state = {
-            email: '',
-            password: '',
-            name: '',
-            isLoading: false
-        }
+    const onInputChange = (key) => (event) => {
+        setUser( prevState => ({ 
+            ...prevState, 
+            [key]: event.target.value 
+        }));
     }
 
-    onInputChange = (key) => (event) => {
-        this.setState({ [key]: event.target.value });
-    }
-
-    handleSubmit = async (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        const { email, password, name } = this.state;
-        const { route } = this.props;
-
-        this.setState({ isLoading: true });
+        const { email, password, name } = user;
+        
+        setLoading(true);
         
         const response = await fetch(`https://guarded-crag-64592.herokuapp.com/${route}`, {
             method: 'POST',
@@ -43,126 +42,123 @@ class SignForm extends Component {
                 name
             })
         })
-        const user = await response.json();
+        const userData = await response.json();
         
-        if (user.id) {
-            this.props.loadUser(user);
-            this.props.onRouteChange('home');
+        if (userData.id) {
+            loadUser(userData);
+            onRouteChange('home');
         }
     };
 
-    render() {
-        const { route, onRouteChange } = this.props;
-        const title = route === 'register' ? 'Register' : 'Sign In';
+    const title = route === 'register' ? 'Register' : 'Sign In';
 
-        if (this.state.isLoading)
-            return <div className='loading'></div>
+    if (isLoading)
+        return <div className='loading'></div>
 
-        return (
-            <Container component="main" maxWidth="sm">
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        p: 5, 
-                        borderRadius: 2, 
-                        boxShadow: 2
-                    }}
-                    bgcolor="white"
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                    {title}
-                    </Typography>
-                    <Box component="form" onSubmit={this.handleSubmit} noValidate sx={{ mt: 1, }} >
-                        {
-                            route === 'register' 
-                            ?                         
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="name"
-                                label="Name"
-                                name="name"
-                                autoComplete="name"
-                                autoFocus
-                                sx={{ "& .MuiInputBase-root": {
-                                height: 50,
-                                } }}
-                                onChange={this.onInputChange('name')}
-                            />
-                            : ''
-                        }
+    return (
+        <Container component="main" maxWidth="sm">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    p: 5, 
+                    borderRadius: 2, 
+                    boxShadow: 2
+                }}
+                bgcolor="white"
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                {title}
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, }} >
+                    {
+                        route === 'register' 
+                        ?                         
                         <TextField
                             margin="normal"
                             required
                             fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
+                            id="name"
+                            label="Name"
+                            name="name"
+                            autoComplete="name"
                             autoFocus
                             sx={{ "& .MuiInputBase-root": {
                             height: 50,
                             } }}
-                            onChange={this.onInputChange('email')}
+                            onChange={onInputChange('name')}
                         />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            sx={{ "& .MuiInputBase-root": {
-                            height: 50
-                            } }}
-                            onChange={this.onInputChange('password')}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2, pt: 1.2, pb: 1.2}}
-                        >
-                            {title}
-                        </Button>
-                        {
-                             route === 'register' 
-                             ?                         
-                             <Grid container justifyContent="flex-end">
-                                <Grid item>
-                                    <Link href="#" variant="body2" onClick={() => onRouteChange('signin')}>
-                                        {"Already have an account? Sign in"}
-                                    </Link>
-                                </Grid>
+                        : ''
+                    }
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        sx={{ "& .MuiInputBase-root": {
+                        height: 50,
+                        } }}
+                        onChange={onInputChange('email')}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        sx={{ "& .MuiInputBase-root": {
+                        height: 50
+                        } }}
+                        onChange={onInputChange('password')}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2, pt: 1.2, pb: 1.2}}
+                    >
+                        {title}
+                    </Button>
+                    {
+                            route === 'register' 
+                            ?                         
+                            <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="#" variant="body2" onClick={() => onRouteChange('signin')}>
+                                    {"Already have an account? Sign in"}
+                                </Link>
                             </Grid>
-                             : 
-                             <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="#" variant="body2" onClick={() => onRouteChange('register')}>
-                                        {"Don't have an account? Register"}
-                                    </Link>
-                                </Grid>
+                        </Grid>
+                            : 
+                            <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
                             </Grid>
-                        }
-                    </Box>
+                            <Grid item>
+                                <Link href="#" variant="body2" onClick={() => onRouteChange('register')}>
+                                    {"Don't have an account? Register"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    }
                 </Box>
-            </Container>
-        );
-    }
+            </Box>
+        </Container>
+    );
 }
 
 export default SignForm;
